902e626cc58945c54c0ed4ea399e42a106bd21f3
saito.k <saito.k.ch@m.titech.ac.jp>
2026-01-29 13:58:58 +0900

    Update to be more useful

--- scripts/qsub/qsub_all_sequential.sh
+++ scripts/qsub/qsub_all_sequential.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # How-to-use:
-# 1. Modify MODEL_NAME in qsub_all.sh to $1 respectively to receive arguments from this script.
+# 1. Modify corresponding variables in qsub_all.sh to $1, $2, ... respectively to receive arguments from this script.
 # 2. Set other parameters in qsub_all.sh as needed.
 # 3. Copy and paste the target models given in a request for evaluation into the target_models array.
 # 4. Run this code by `bash scripts/qsub/qsub_all_sequential.sh`.
@@ -16,6 +16,11 @@ dummy_model/we_hope_this_code_is_useful_for_you
 dummy_model/have_a_nice_day
 )
 
+node_kind="node_q"
+
+custom_setting=""
+
 for model in ${target_models[@]}; do
-    bash scripts/qsub/qsub_all.sh $model
+    qsub_args=("${node_kind}" "${model}" "${custom_setting}")
+    bash scripts/qsub/qsub_all.sh "${qsub_args[@]}"
 done
