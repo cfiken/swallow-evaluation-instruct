d82a0d7d959f71b89da8655b834a10beccaa0580
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2026-03-21 18:13:28 +0900

    feat: 🎸 added PolyMath-Ja metrics to agg. script

--- scripts/aggregate_utils/conf.py
+++ scripts/aggregate_utils/conf.py
@@ -1276,5 +1276,33 @@ AGGREGATE_CONF += [
             'task_key': 'swallow|ifbench_singleturn|0'
         }
     },
+    # PolyMath-Ja
+    # 難易度 medium, high, top および high+top 平均の4指標を追加
+    {
+        "display_name": "polymath_ja_medium",
+        "func": pick,
+        "func_args": {'metric_key': 'extractive_match'}, 
+        'target': {'task_key': 'swallow|polymath_japanese:medium|0'}
+    },
+    {
+        "display_name": "polymath_ja_high",
+        "func": pick,
+        "func_args": {'metric_key': 'extractive_match'}, 
+        'target': {'task_key': 'swallow|polymath_japanese:high|0'}
+    },
+    {
+        "display_name": "polymath_ja_top",
+        "func": pick,
+        "func_args": {'metric_key': 'extractive_match'}, 
+        'target': {'task_key': 'swallow|polymath_japanese:top|0'}
+    },
+    {
+        "display_name": "polymath_ja_high_top",
+        "func": micro_average,
+        "func_args": {
+            'metric_key': 'extractive_match',
+            "white_list": ["high", "top"]}, 
+        'target': {'task_key': 'swallow|polymath_japanese|0'}
+    },
     # 新しいベンチマークはここに追加していってください
 ]
